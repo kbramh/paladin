7fd35766adf0bf4825f0ef4dcaf30172ffd920da
kbramh <kylebramhall2@hotmail.com>
2026-05-23 22:19:20 -0700

    converted html files to react

---
+++ src/faqs.tsx
@@ -0,0 +1,51 @@
+import { createRoot } from "react-dom/client";
+import Header from "./components/Header";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <>
+    <Header />
+    <div className="faq-container">
+      <h2>Frequently Asked Questions</h2>
+      <p className="subtitle">Commonly asked questions and answers:</p>
+
+      <ul className="faq-list">
+        <li>
+          <strong>What types of fraud can I report?</strong>
+          <span className="answer">
+            You can report any type of suspected fraud related to businesses - whether they are engaging in illegal
+            activity that effects you or not. Some examples include scams, running illegal fenced goods operations, etc.
+          </span>
+        </li>
+        <li>
+          <strong>How do I submit a fraud report?</strong>
+          <span className="answer">
+            Click the "Report Fraud" button at the top of the page in the navigation bar, then click the green "proceed"
+            button after you read the text on that page.You’ll receive a confirmation email with a report reference
+            number.
+          </span>
+        </li>
+        <li>
+          <strong>Is my personal information kept confidential?</strong>
+          <span className="answer">
+            Yes. All reports are handled securely and confidentially. We comply with strict privacy laws and never share
+            your information without your consent.
+          </span>
+        </li>
+        <li>
+          <strong>How long does it take to process my report?</strong>
+          <span className="answer">
+            Most reports are reviewed within 3-5 business days. Complex cases may take longer. You can track the status
+            of your report using your report reference number.
+          </span>
+        </li>
+        <li>
+          <strong>Can I report fraud anonymously?</strong>
+          <span className="answer">
+            No, you cannot submit a report without providing contact details (phone number or email).
+          </span>
+        </li>
+      </ul>
+    </div>
+  </>,
+);
---
+++ src/reportFraudWebReferralForm.tsx
@@ -0,0 +1,73 @@
+import { createRoot } from "react-dom/client";
+import Header from "./components/Header";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <>
+    <Header />
+    <main className="mainbody">
+      <div className="body-content-main-container" id="divBodyContent">
+        <div className="panel-heading">
+          <ol className="breadcrumb">
+            <li>
+              <a href="#" title="Start" className="breadcrumb">
+                Start
+              </a>
+            </li>
+            <li>
+              <span className="breadcrumb">/ Vendor Details /</span>
+            </li>
+            <li>
+              <span className="breadcrumb">Fraud Details /</span>
+            </li>
+            <li>
+              <span className="breadcrumb">Referrer's Information /</span>
+            </li>
+            <li>
+              <span className="breadcrumb">Review & Submit</span>
+            </li>
+          </ol>
+        </div>
+        <form>
+          <div className="panel-body" id="divMain">
+            <div className="form-horizontal"></div>
+          </div>
+        </form>
+      </div>
+    </main>
+    <div className="business-form-container" id="businessFormContent">
+      <div className="horizontal-form" id="report-form">
+        <div className="table-sections">
+          <div className="vendorName section"></div>
+          <div className="vendorName rows">
+            <div className="vendorName field"></div>
+            <div className="alternative names field"></div>
+          </div>
+          <br />
+          <div className="vendorAddress section"></div>
+          <div className="row divAddressInformation">
+            <div className="street field"></div>
+            <div className="street2 field"></div>
+            <div className="unit field"></div>
+          </div>
+          <div className="row divAddressInformation">
+            <div className="city field"></div>
+            <div className="state field"></div>
+            <div className="zip field"></div>
+          </div>
+          <div className="row divAddressInformation">
+            <div className="county field"></div>
+            <div className="country field"></div>
+          </div>
+          <div className="row divAddressInformation">
+            <div className="phoneNumber field"></div>
+            <div className="email field"></div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="bottom-section"></div>
+      </div>
+    </div>
+  </>,
+);
